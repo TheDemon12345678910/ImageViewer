6d05b93e01ad011af68d32fe9c206fd36802e6b6
TheDemon12345678910 <anehoimark@gmail.com>
2023-03-20 12:18:48 +0100

    addedSlideShowBtn

---
+++ src/dk/easv/SlideShow.java
@@ -0,0 +1,13 @@
+package dk.easv;
+
+public class SlideShow implements Runnable{
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

7336e2b13f8c6801a7b7765046dcce507e974ea2
Miran K <mirkom01@easv365.dk>
2023-03-27 10:41:30 +0200

    images easier

--- src/dk/easv/ImageViewerWindowController.java
+++ src/dk/easv/ImageViewerWindowController.java
@@ -34,6 +34,7 @@ public class ImageViewerWindowController
     {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select image files");
+        fileChooser.setInitialDirectory(new File("C:\\Users\\Miran\\ImageViewer\\data\\Images"));
         fileChooser.getExtensionFilters().add(new ExtensionFilter("Images",
                 "*.png", "*.jpg", "*.gif", "*.tif", "*.bmp"));
         List<File> files = fileChooser.showOpenMultipleDialog(new Stage());
